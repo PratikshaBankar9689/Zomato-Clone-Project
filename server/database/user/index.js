3ec7971fa11b0694e18a2e02b24ccd318797fc58
PratikshaBankar9689 <pratikshabankar5313@gmail.com>
2022-10-07 14:27:14 +0530

    created user api

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -13,12 +15,53 @@ const UserSchema = new mongoose.Schema(
   }
 );
 
-// attachments
-UserSchema.methods.generateJwtToken = function () {};
+UserSchema.methods.generateJwtToken = function () {
+  return jwt.sign({ user : this._toString()},"ZomatoApp");
+};
 
-// helper functions
-UserSchema.statics.findByEmailAndPhone = async () => {};
 
-UserSchema.statics.findByEmailAndPassword = async () => {};
+UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
+  const checkUserByEmail = await UserModel.findOne({ email });
+  const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+
+  if (checkUserByEmail || checkUserByPhone) {
+    throw new Error("User Already Exists ...!");
+  }
+
+  return false;
+};
+
+UserSchema.statics.findByEmailAndPassword = async ({ email, password }) => {
+  const user = await UserModel.findOne({ email });
+  if (!user) throw new Error("User does not exist....");
+
+  
+  const doesPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!doesPasswordMatch) throw new Error("Invalid Credentials....");
+
+  return user;
+};
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  
+  bcrypt.genSalt(8, (error, salt) => {
+    if (error) return next(error);
+
+    
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+
+      
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
 
 export const UserModel = mongoose.model("users", UserSchema);
